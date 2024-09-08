211f24551e242f461b80032141630fc378dc8ce7
Sergei Bozhko <sbozhko@mpi-sws.org>
2024-09-08 22:42:25 +0200

    remove canvas effects that I will not use

--- client/src/components/game/canvas.tsx
+++ client/src/components/game/canvas.tsx
@@ -1,56 +1,20 @@
-import React, { ReactNode, useCallback, useEffect, useState } from "react";
+import React from "react";
 import { Canvas } from "@react-three/fiber";
+import { EffectComposer, Vignette } from "@react-three/postprocessing";
 import { OrbitControls, Environment } from "@react-three/drei";
 import Arena from "src/components/game/arena";
 
-import * as THREE from "three";
-import {
-  ChromaticAberration,
-  ColorAverage,
-  EffectComposer,
-  Noise,
-  Pixelation,
-  Vignette,
-} from "@react-three/postprocessing";
-import { BlendFunction } from "postprocessing";
-import { Perf } from "r3f-perf";
-import { useParams } from "react-router-dom";
-
 // TODO: function --> const
 export default function PylosCanvas() {
-  let { id } = useParams();
-
-  const [effects, setEffects] = useState(false);
-
   return (
     <div className="w-full h-full">
       <Canvas shadows camera={{ position: [-8, 8, 0] }}>
-        {/* <Perf /> */}
         <Environment background preset="forest" backgroundBlurriness={1} />
         <ambientLight intensity={0.9} />
         <Arena />
-        {effects ? (
-          <EffectComposer>
-            <Vignette eskil={false} offset={0} darkness={1.2} />
-
-            <ColorAverage
-              blendFunction={BlendFunction.NORMAL} // blend mode
-            />
-            <Pixelation granularity={2} />
-
-            <ChromaticAberration
-              blendFunction={BlendFunction.NORMAL}
-              offset={new THREE.Vector2(0.005, 0.002)}
-              radialModulation={false}
-              modulationOffset={0}
-            />
-            <Noise premultiply blendFunction={BlendFunction.AVERAGE} />
-          </EffectComposer>
-        ) : (
-          <EffectComposer>
-            <Vignette eskil={false} offset={0} darkness={1} />
-          </EffectComposer>
-        )}
+        <EffectComposer>
+          <Vignette eskil={false} offset={0} darkness={1} />
+        </EffectComposer>
         <OrbitControls
           enableDamping={true}
           enableZoom={false}
